acf4b9d7126ae4e1f91d200b1c4a9e7aa4000b35
pitman2e <pitman2e@gmail.com>
2022-01-12 18:40:57 +0800

    test

--- setup.py
+++ setup.py
@@ -20,7 +20,7 @@ setuptools.setup(
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
     ],
-    packages=['pydict', 'yahoo_dict'],
+    packages=['pydict'],
     package_dir={"pydict": "py_src/pydict"},
     package_data={'pydict': ['ui/dict.ui']},
     python_requires=">=3.8",
