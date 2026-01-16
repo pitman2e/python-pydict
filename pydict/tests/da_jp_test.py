2ad5f8b85b13be496df5094654635ff4da89c69e
pitman2e <pitman2e@gmail.com>
2026-01-17 00:24:33 +0800

    Use clear test text

--- pydict/tests/da_jp_test.py
+++ pydict/tests/da_jp_test.py
@@ -5,43 +5,43 @@ import pydict.da_jp
 
 class TestSc2Tc(unittest.TestCase):
     def test_sc2tc_to_traditional_1(self):
-        txt = "あい简えお"
+        txt = "あい体えお"
         results = []
         pydict.da_jp.to_traditional(txt)
 
-        self.assertEqual(pydict.da_jp.to_traditional(txt), "あい简えお")
+        self.assertEqual("あい体えお", pydict.da_jp.to_traditional(txt))
 
 
     def test_sc2tc_to_traditional_2(self):
-        txt = "あい简えお。简简简" #LHS: Japanese / RHS: SC
+        txt = "あい体えお。体" #LHS: Japanese / RHS: SC
         results = []
         pydict.da_jp.to_traditional(txt)
 
-        self.assertEqual(pydict.da_jp.to_traditional(txt), "あい简えお。簡簡簡")
+        self.assertEqual("あい体えお。體", pydict.da_jp.to_traditional(txt))
 
 
     def test_sc2tc_part_definition_1(self):
-        txt = "2.（简）简简（簡単な日本語。）"
+        txt = "2.（体）体体（体力日本語。）"
         results = []
         pydict.da_jp.sc2tc_part_definition(txt, results)
 
-        self.assertEqual(results[0], "2.（簡）簡簡（簡単な日本語。）")
+        self.assertEqual("2.（體）體體（体力日本語。）", results[0])
 
 
     def test_sc2tc_part_definition_2(self):
         # 当てつけ
-        txt = "あい简えお"
+        txt = "あい体えお"
         results = []
         pydict.da_jp.sc2tc_part_definition(txt, results)
 
-        self.assertEqual(results[0], txt)
+        self.assertEqual("あい体えお", results[0])
 
 
     def test_sc2tc_part_definition_3(self):
         # 当てつけ
-        txt = "简简, 简简"
+        txt = "体体, 体体"
         results = []
         pydict.da_jp.sc2tc_part_definition(txt, results)
 
-        self.assertEqual(results[0], "簡簡, 簡簡")
+        self.assertEqual(results[0], "體體, 體體")
 
