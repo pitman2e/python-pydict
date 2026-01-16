9a28db9d78c5c7820d6f822bff7f5937e10cec88
pitman2e <pitman2e@gmail.com>
2026-01-17 00:18:18 +0800

    Test casese for to_traditional

--- pydict/tests/da_jp_test.py
+++ pydict/tests/da_jp_test.py
@@ -4,7 +4,23 @@ import pydict.da_jp
 
 
 class TestSc2Tc(unittest.TestCase):
-    def test_sc2tc_part_definition(self):
+    def test_sc2tc_to_traditional_1(self):
+        txt = "あい简えお"
+        results = []
+        pydict.da_jp.to_traditional(txt)
+
+        self.assertEqual(pydict.da_jp.to_traditional(txt), "あい简えお")
+
+
+    def test_sc2tc_to_traditional_2(self):
+        txt = "あい简えお。简简简" #LHS: Japanese / RHS: SC
+        results = []
+        pydict.da_jp.to_traditional(txt)
+
+        self.assertEqual(pydict.da_jp.to_traditional(txt), "あい简えお。簡簡簡")
+
+
+    def test_sc2tc_part_definition_1(self):
         txt = "2.（简）简简（簡単な日本語。）"
         results = []
         pydict.da_jp.sc2tc_part_definition(txt, results)
